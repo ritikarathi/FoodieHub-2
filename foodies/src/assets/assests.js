b9652eb7d32d3cb41ea0c047c1eb288c484fac4c
Ritika <ritikarathi2005@gmail.com>
2026-06-29 19:09:55 +0530

    update in ExploreMenu

--- foodies/src/assets/assests.js
+++ foodies/src/assets/assests.js
@@ -1,7 +1,43 @@
 import logo from './logo.png'
 import cart from './cart.png'
 
+import burger from './b1.png';
+import beverages from './bev3.png';
+import pasta from './pa1.png';
+import pizza from './p1.png';
+import desserts from './d2.png';
+import cake from './c2.png';
+
+
 export const assests ={
     logo,
     cart
 }
+
+export const categories=[
+    {
+        category: 'Burger',
+        icon: burger
+    },
+    {
+        category: 'Beverages',
+        icon: beverages
+    },
+    {
+        category: 'Pasta',
+        icon: pasta
+    },
+    {
+        category: 'Cake',
+        icon: cake
+    },
+    {
+        category: 'Desserts',
+        icon: desserts
+    },
+    {
+        category: 'Pizza',
+        icon: pizza
+    },
+    
+]
--- foodies/src/components/ExploreMenu/ExploreMenu.jsx
+++ foodies/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,30 @@
 
-
+import {categories} from "../../assets/assests"
+import './ExploreMenu.css'
 const ExploreMenu = () => {
   return (
-    <div>
-      Explore Menu
+    <div className="explore-menu position-relative">
+        <h1 className="d-flex align-items-center justify-content-between">
+            Explore Our Menu
+            <div className="d-flex">
+            <i className="bi bi-arrow-left-circle scroll-icon"></i>
+            <i className="bi bi-arrow-right-circle scroll-icon"></i>
+            </div>
+        </h1>
+        <p>Explore Curated Lists of Dishes From Top Categories</p>
+        <div className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list">
+            {
+                categories.map((item,index)=>{
+                    return (
+                        <div key={index} className="text-center explore-menu-list-item">
+                            <img src={item.icon} alt="" className="rounded-circle" height={130} width={130} />
+                            <p className="mt-2 fw-bold">{item.category}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        
     </div>
   )
 }
